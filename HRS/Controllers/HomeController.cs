e304a3146800c28927d6d79aaff6f097bcf0f64d
Ümit <umit.haciyusufoglu@gmail.com>
2019-04-25 00:11:57 +0300

    dependencies

--- HRS/Controllers/HomeController.cs
+++ HRS/Controllers/HomeController.cs
@@ -2,14 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HRS.Data;
+using HRS.Helpers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HRS
 {
     public class HomeController : Controller
     {
+        private readonly ManagerContext context;
+        private readonly IUserManager userManager;
+
+        public HomeController(ManagerContext _context, IUserManager _userManager)
+        {
+            context = _context;
+            userManager = _userManager;
+        }
+
         public IActionResult Index()
         {
+            HttpContext.Session.
             return View();
         }
 
