7bb05cecfd2a277ca5ea3b430c3cb39554f19efa
Ümit <umit.haciyusufoglu@gmail.com>
2019-04-24 21:41:39 +0300

    Araçlar ve RoleConfig

---
+++ HRS/Helpers/Utils.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRS.Helpers
+{
+    public class Utils
+    {
+        public static class RoleConfig
+        {
+            public static readonly string Founder = "Developer";
+            public static readonly string Admin = "Admin";
+            public static readonly string Operator = "Operator";
+            public static readonly string User = "User";
+        }
+
+        public static class HashString
+        {
+            public static string GetMD5(string text)
+            {
+                MD5 hash = MD5.Create();
+
+                byte[] data = hash.ComputeHash(Encoding.UTF8.GetBytes(text));
+
+                StringBuilder builder = new StringBuilder();
+
+                for (int i = 0; i < data.Length; i++)
+                {
+                    builder.Append(data[i].ToString("x2"));
+                }
+
+                hash.Clear();
+
+                return builder.ToString();
+            }
+        }
+    }
+}
