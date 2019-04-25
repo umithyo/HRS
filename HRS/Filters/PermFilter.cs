0445434a1221e99f808dae5f497835dcd6bdbe2e
Ümit <umit.haciyusufoglu@gmail.com>
2019-04-25 23:42:09 +0300

    Authorization filter added.

---
+++ HRS/Filters/PermFilter.cs
@@ -0,0 +1,40 @@
+﻿using HRS.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using static HRS.Helpers.Utils;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace HRS.Filters
+{
+
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+    public class PermissionAuthorize : ActionFilterAttribute, IActionFilter
+    {
+        public string[] Permissions { get; set; }
+        public string RedirectUri { get; set; }
+
+        public PermissionAuthorize():base()
+        {   
+            
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var sessionManager = context.HttpContext.RequestServices.GetService<ISessionManager>();
+            var userRole = sessionManager.GetRole();
+            if (!Permissions.Any(x => x == userRole))
+            {
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                context.HttpContext.Response.Redirect(!string.IsNullOrEmpty(RedirectUri) ? RedirectUri : "/Home/Login");
+            }
+        }    
+    }
+}
