a8770819fef02dffabfa75ee53811e31fff415c4
Ümit <umit.haciyusufoglu@gmail.com>
2019-04-26 22:28:35 +0300

    Viewmodels

---
+++ HRS/Models/ViewModels/LoginVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRS.Models.ViewModels
+{
+    public class LoginVM
+    {
+        public User User { get; set; }
+        public UserInfo UserInfo { get; set; }
+    }
+}
