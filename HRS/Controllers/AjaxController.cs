b541c4d4eac3d666351c2eee2839b65940a624c5
Ümit <umit.haciyusufoglu@gmail.com>
2019-04-28 07:42:10 +0300

    added ajax controller for ajax requests

---
+++ HRS/Controllers/AjaxController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HRS.Data;
+using HRS.Filters;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRS.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    [PermissionAuthorize]
+    public class AjaxController : ControllerBase
+    {
+        private readonly ManagerContext context;
+
+        public AjaxController(ManagerContext _context)
+        {
+            context = _context;
+        }
+
+        [HttpGet("GetTowns/{id}")]
+        public IActionResult GetTowns(int? id)
+        {
+            if (id == null)
+                return BadRequest("Hatalı id.");               
+            var towns = context.Towns.Where(x => x.City.Id == id).ToList();
+            return Ok(towns);
+        }
+    }
+}
