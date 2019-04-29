0ab3d9aa2bf920a1a71487a0c3b1afac632d4cd8
Ümit <umit.haciyusufoglu@gmail.com>
2019-04-29 06:29:49 +0300

    Added management controller

---
+++ HRS/Controllers/ManagementController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HRS.Data;
+using HRS.Filters;
+using HRS.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static HRS.Helpers.Utils;
+
+namespace HRS.Controllers
+{
+    //[PermissionAuthorize(Permissions = RoleConfig.Admin, UnauthorizedRedirectUri = "/Home/Index")]
+    public class ManagementController : Controller
+    {
+        private readonly ManagerContext context;
+        private readonly ISessionManager sessionManager;
+        private readonly IHospitalManager hospitalManager;
+        public ManagementController(ManagerContext _context, ISessionManager _sessionManager, IHospitalManager _hospitalManager)
+        {
+            context = _context;
+            sessionManager = _sessionManager;
+            hospitalManager = _hospitalManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Hospitals()
+        {
+            ViewBag.Cities = context.Cities.ToList();
+            return View();
+        }
+    }
+}
