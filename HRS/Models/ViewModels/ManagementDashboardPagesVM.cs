5f3b7af4663eeef5d4c4d780e9d29ab867674c5b
Ümit <umit.haciyusufoglu@gmail.com>
2019-04-29 06:33:13 +0300

    added VM for easier layout loading

---
+++ HRS/Models/ViewModels/ManagementDashboardPagesVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRS.Models.ViewModels
+{
+    public class ManagementDashboardPagesVM
+    {
+        public string Header { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+        public string BackgroundClass { get; set; }
+        public string Action { get; set; }
+        
+    }
+}
