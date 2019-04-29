416e154590088813bc4f4d2f64867352ce8eeef6
Ümit <umit.haciyusufoglu@gmail.com>
2019-04-29 06:31:45 +0300

    added HospitalManager service

---
+++ HRS/Helpers/HospitalManager.cs
@@ -0,0 +1,41 @@
+﻿using HRS.Data;
+using HRS.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRS.Helpers
+{
+    public interface IHospitalManager
+    {
+        ManagerStatus CreateHospital(Hospital hospital);
+    }
+
+    public class HospitalManager:IHospitalManager
+    {
+        private IHttpContextAccessor accessor;
+        private HttpContext HttpContext;
+        private readonly ManagerContext context;
+        private readonly ISessionManager sessionManager;
+
+        public HospitalManager(IHttpContextAccessor _accessor, ManagerContext _context, ISessionManager _sessionManager)
+        {
+            accessor = _accessor;
+            HttpContext = accessor.HttpContext;
+            context = _context;
+            sessionManager = _sessionManager;
+        } 
+
+        public ManagerStatus CreateHospital(Hospital hospital)
+        {
+            if (context.Hospitals.Any(x => x.Name == hospital.Name))
+                return ManagerStatus.UNKNOWN;
+            hospital.CreatedAt = DateTime.Now;
+            context.Add(hospital);
+            context.SaveChanges();
+            return ManagerStatus.OK;
+        }
+    }
+}
