7aefd9e5ed352e99534b8406fc8f71b3dfdbf80a
Ümit <umit.haciyusufoglu@gmail.com>
2019-05-11 02:58:28 +0300

    added errors, couple of tweaks

--- HRS/wwwroot/js/globals.js
+++ HRS/wwwroot/js/globals.js
@@ -1,20 +1,14 @@
 ﻿$(document).ready(function () {
-    function regDependentSelect(dependeeDOMId, dependantDOMId, url, dependeeFirstOption, dependantFirstOption, twoWay) {
+    //only use if there are two dependencies in other words, two select boxes.
+    function regDependentSelect(dependeeDOMId, dependantDOMId, url, dependeeFirstOption, dependantFirstOption) {
         $(dependeeDOMId).on('change', function () {
             $(dependantDOMId).html("<option>" + dependantFirstOption + "</option>");
-            if (this.value != dependeeFirstOption) {
-                if (twoWay == null) {
-                    $(dependantDOMId).html("<option>Yükleniyor...</option>");
-                }
+            if (this.value !== dependeeFirstOption) {
+                $(dependantDOMId).html("<option>Yükleniyor...</option>");
                 $.ajax({
                     url: url + "/" + this.value,
                     success: function (data) {
-                        if (twoWay == null) {
-                            $(dependantDOMId).html("<option>" + dependantFirstOption + "</option>");
-                        } else {
-                            let current = $(dependantDOMId).find("option").children();
-                            console.log(current);
-                        }
+                        $(dependantDOMId).html("<option>" + dependantFirstOption + "</option>");
 
                         $.each(data, function () {
                             $(dependantDOMId).append($("<option />").val(this.id).text(this.name));
